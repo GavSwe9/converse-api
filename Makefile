b8cbf703818f9d19054e415f0a452ff3c84461a8
Gavin Sweeney <gavswe19@gmail.com>
2022-12-16 23:00:08 -0500

    Create message handler

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 build:
 	env GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/connectHandler connectHandler/main.go
 	env GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/disconnectHandler disconnectHandler/main.go
+	env GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/messageHandler messageHandler/main.go
 
 clean:
 	rm -rf ./bin
---
+++ messageHandler/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"encoding/json"
+)
+
+type MessageItem struct {
+	GroupId  int    `json:"groupId"`
+	UserName string `json:"userName"`
+	Message  string `json:"message"`
+}
+
+func (e *MessageItem) Decode(data []byte) (*MessageItem, error) {
+	err := json.Unmarshal(data, e)
+	return e, err
+}
+
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context, request events.APIGatewayWebsocketProxyRequest) (Response, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	fmt.Println(ctx)
+	fmt.Println(request.Body)
+
+	svc := dynamodb.New(sess)
+	
+	messageItem, err := new(MessageItem).Decode([]byte(request.Body))
+
+	dynamoItem, err := dynamodbattribute.MarshalMap(messageItem)
+	if err != nil {
+		log.Fatalf("Error marshalling new connection item: %s", err)
+	}
+
+	tableName := os.Getenv("MESSAGES_TABLE")
+
+	input := &dynamodb.PutItemInput{
+		Item:      dynamoItem,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Error calling PutItem: %s", err)
+	}
+
+	fmt.Println("Successfully added connection to table")
+
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+	fmt.Println(resp)
+	return resp, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
